690b6712ed5149a41d75b3412a7b01c345fcaa10
ameziel <samuel.sicard.it@gmail.com>
2023-01-29 17:05:11 +0100

    Init hello world project

---
+++ src/main/java/tms/tennismanagementsystem/TennisManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package tms.tennismanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TennisManagementSystemApplication.class, args);
+        System.out.println("Hello world");
+    }
+
+}
---
+++ src/test/java/tms/tennismanagementsystem/TennisManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package tms.tennismanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
