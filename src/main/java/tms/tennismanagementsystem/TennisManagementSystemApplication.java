2b6e4fd6fd434f4cdedf8002dd6854e560ed6cce
ameziel <samuel.sicard.it@gmail.com>
2023-01-30 18:40:40 +0100

    Back-end: Security config part1

--- src/main/java/tms/tennismanagementsystem/TennisManagementSystemApplication.java
+++ src/main/java/tms/tennismanagementsystem/TennisManagementSystemApplication.java
@@ -8,7 +8,6 @@ public class TennisManagementSystemApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TennisManagementSystemApplication.class, args);
-        System.out.println("Hello world");
     }
 
 }
