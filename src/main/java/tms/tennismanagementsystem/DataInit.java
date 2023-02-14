c396603829bef267f5ef90f90ce868672633a882
ameziel <samuel.sicard.it@gmail.com>
2023-02-15 00:22:51 +0100

    Classe pour initialiser les valeurs de départs

---
+++ src/main/java/tms/tennismanagementsystem/DataInit.java
@@ -0,0 +1,23 @@
+package tms.tennismanagementsystem;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tms.tennismanagementsystem.app.student.EleveEntity;
+import tms.tennismanagementsystem.app.student.EleveRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Configuration
+public class DataInit {
+
+    @Bean
+    CommandLineRunner commandLineRunner(EleveRepository eleveRepository) {
+        return args -> {
+            EleveEntity first = new EleveEntity("Sam", "Fadatare", "+32481098012", "ramesh@gmail.com", LocalDateTime.now());
+            EleveEntity second = new EleveEntity("Samy", "Fadatare", "+32481098012", "ramesh@gmail.com", LocalDateTime.now());
+            eleveRepository.saveAll(List.of(first,second));
+        };
+    }
+}
