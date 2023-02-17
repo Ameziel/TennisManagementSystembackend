892379ac881918246c06113127f04719f4e30369
ameziel <samuel.sicard.it@gmail.com>
2023-02-17 14:50:26 +0100

    EleveController v1

---
+++ src/main/java/tms/tennismanagementsystem/app/student/EleveController.java
@@ -0,0 +1,45 @@
+package tms.tennismanagementsystem.app.student;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import tms.tennismanagementsystem.app.student.exceptions.EleveNotFoundException;
+
+import java.util.List;
+import java.util.UUID;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping(path = "/api")
+public class EleveController {
+
+    private EleveService eleveService;
+
+    @GetMapping("/eleves")
+    public List<EleveDTO> getAllEleves() {
+        return eleveService.getAllEleves();
+    }
+
+    @GetMapping("/eleves/{id}")
+    public EleveDTO getEleve(@PathVariable(name = "id")String id) throws EleveNotFoundException {
+        return eleveService.getEleveById(UUID.fromString(id));
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/eleves")
+    public EleveDTO save(@RequestBody EleveDTO eleveDTO) {
+        return eleveService.saveEleve(eleveDTO);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/eleves/{id}")
+    public EleveDTO updateEleve(@PathVariable UUID id, @RequestBody EleveDTO eleveDTO) {
+        eleveDTO.setId(id);
+        return eleveService.updateEleve(eleveDTO);
+    }
+    @ResponseStatus()
+    @DeleteMapping("/eleves/{id}")
+    public void deleteEleve(@PathVariable UUID id) {
+        eleveService.deleteEleve(id);
+    }
+}
