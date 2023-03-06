a4e6b763e8e216406c5c02bdf1d039d2d4ac076e
ameziel <samuel.sicard.it@gmail.com>
2023-03-06 23:48:29 +0100

    Ajout exception d'une "FormuleCours" non trouvé

---
+++ src/main/java/tms/tennismanagementsystem/app/formulecours/exceptions/FormuleNotFoundException.java
@@ -0,0 +1,6 @@
+package tms.tennismanagementsystem.app.formulecours.exceptions;
+
+public class FormuleNotFoundException extends Exception {
+    public FormuleNotFoundException(String formule_not_found) {
+    }
+}
