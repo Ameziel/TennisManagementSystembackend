9539e88b0bf88cde5a4287f01bd339cc53082ea9
ameziel <samuel.sicard.it@gmail.com>
2023-03-06 23:47:25 +0100

    Ajout v1 du service FormuleCours

---
+++ src/main/java/tms/tennismanagementsystem/app/formulecours/FormuleCoursService.java
@@ -0,0 +1,15 @@
+package tms.tennismanagementsystem.app.formulecours;
+
+import tms.tennismanagementsystem.app.formulecours.exceptions.FormuleNotFoundException;
+import java.util.List;
+import java.util.UUID;
+
+public interface FormuleCoursService {
+    List<FormuleCoursDTO> getAllFormulesCours();
+    FormuleCoursDTO getFormuleCoursById(UUID id) throws FormuleNotFoundException; // throws exception
+    FormuleCoursDTO saveFormuleCours(FormuleCoursDTO formuleCoursDTO);
+    FormuleCoursDTO updateFormuleCours(FormuleCoursDTO formuleCoursDTO);
+    void deleteFormuleCours(UUID id);
+
+
+}
