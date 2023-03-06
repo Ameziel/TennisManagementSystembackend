5bb3ed39d296e5d25a900947a48fb6bd8007f719
ameziel <samuel.sicard.it@gmail.com>
2023-03-06 23:47:52 +0100

    Ajout v1 de l'implémentation du service

---
+++ src/main/java/tms/tennismanagementsystem/app/formulecours/FormuleCoursServiceImplementation.java
@@ -0,0 +1,46 @@
+package tms.tennismanagementsystem.app.formulecours;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import tms.tennismanagementsystem.app.formulecours.exceptions.FormuleNotFoundException;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class FormuleCoursServiceImplementation implements FormuleCoursService {
+
+    private FormuleCoursRepository formuleCoursRepository;
+    private FormuleCoursMapperImplementation formuleCoursMapperImplementation;
+    @Override
+    public List<FormuleCoursDTO> getAllFormulesCours() {
+        List<FormuleCoursEntity> formulesCoursEntities = formuleCoursRepository.findAll();
+        List<FormuleCoursDTO> formuleCoursDTOS = formulesCoursEntities.stream().map(
+                formuleCoursEntity -> formuleCoursMapperImplementation.fromFormuleCoursEntity(formuleCoursEntity)).collect(Collectors.toList());
+        return formuleCoursDTOS;
+    }
+    @Override
+    public FormuleCoursDTO getFormuleCoursById(UUID id) throws FormuleNotFoundException {
+        FormuleCoursEntity formuleCoursEntity = formuleCoursRepository.findById(id)
+                .orElseThrow(() -> new FormuleNotFoundException("Formule not found"));
+        return formuleCoursMapperImplementation.fromFormuleCoursEntity(formuleCoursEntity);
+    }
+    @Override
+    public FormuleCoursDTO saveFormuleCours(FormuleCoursDTO formuleCoursDTO) {
+        FormuleCoursEntity formuleCoursEntity = formuleCoursMapperImplementation.fromFormuleCoursDTO(formuleCoursDTO);
+        FormuleCoursEntity savedFormuleCours = formuleCoursRepository.save(formuleCoursEntity);
+        return formuleCoursMapperImplementation.fromFormuleCoursEntity(savedFormuleCours);
+    }
+    @Override
+    public FormuleCoursDTO updateFormuleCours(FormuleCoursDTO formuleCoursDTO) {
+        FormuleCoursEntity formuleCoursEntity = formuleCoursMapperImplementation.fromFormuleCoursDTO(formuleCoursDTO);
+        FormuleCoursEntity savedFormuleCours = formuleCoursRepository.save(formuleCoursEntity);
+        return formuleCoursMapperImplementation.fromFormuleCoursEntity(savedFormuleCours);
+    }
+    @Override
+    public void deleteFormuleCours(UUID id) {
+        formuleCoursRepository.deleteById(id);
+    }
+}
