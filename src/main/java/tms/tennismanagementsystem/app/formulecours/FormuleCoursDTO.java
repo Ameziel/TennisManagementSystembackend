949dd452da8dec2ff23b1f94d943c9605a62ee1d
ameziel <samuel.sicard.it@gmail.com>
2023-03-06 23:46:19 +0100

    Ajout du DTO pour les formules de cours

--- src/main/java/tms/tennismanagementsystem/app/formulecours/FormuleCoursDTO.java
+++ src/main/java/tms/tennismanagementsystem/app/formulecours/FormuleCoursDTO.java
@@ -1,11 +1,27 @@
 package tms.tennismanagementsystem.app.formulecours;
 
-public record FormuleCoursDTO(
-    String id,
-    Integer effectifMaximum,
-    Integer nombreSeance,
-    Double tarif,
-    String annee,
-    String saison
-) {
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+public class FormuleCoursDTO {
+    private UUID id;
+    private String libelle;
+    private Integer effectifMaximum;
+    private Integer nombreSeanceTotal;
+    private Integer nombreSeanceHebdomadaire;
+    private String dureeSeance; //1H ou 1H30
+    private Double tarif;
+    private Integer annee;
+    private LocalDate dateDebut;
+    private LocalDate dateFin;
+    private Periode periode;
+    private TypeBalle typeBalle;
 }
+
+
+
+
+
