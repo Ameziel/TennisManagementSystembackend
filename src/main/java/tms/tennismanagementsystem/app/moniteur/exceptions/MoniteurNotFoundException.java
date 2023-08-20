9932236cb9e308495bf0769aebee05c81e3b62d8
ameziel <samuel.sicard.it@gmail.com>
2023-08-20 19:31:06 +0200

    Correction de l'exception de "MoniteurNotFound"

--- src/main/java/tms/tennismanagementsystem/app/moniteur/exceptions/MoniteurNotFoundException.java
+++ src/main/java/tms/tennismanagementsystem/app/moniteur/exceptions/MoniteurNotFoundException.java
@@ -1,6 +1,6 @@
 package tms.tennismanagementsystem.app.moniteur.exceptions;
 
 public class MoniteurNotFoundException extends Exception {
-
-    public MoniteurNotFoundException(String message) { super(message);}
+    private static final String MONITEUR_NOT_FOUND_MESSAGE_EXCEPTION = "Moniteur not found";
+    public MoniteurNotFoundException() { super(MONITEUR_NOT_FOUND_MESSAGE_EXCEPTION);}
 }
