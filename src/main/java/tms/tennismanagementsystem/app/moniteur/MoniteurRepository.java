21959f40b1e48be7bd647186914e06985cf43d76
ameziel <samuel.sicard.it@gmail.com>
2023-08-20 19:31:39 +0200

    Ajout des méthodes "findActifs" et "findInactifs"

--- src/main/java/tms/tennismanagementsystem/app/moniteur/MoniteurRepository.java
+++ src/main/java/tms/tennismanagementsystem/app/moniteur/MoniteurRepository.java
@@ -9,7 +9,8 @@ import java.util.List;
 import java.util.UUID;
 @Repository
 public interface MoniteurRepository extends JpaRepository<MoniteurEntity, UUID> {
-
     @Query("FROM MoniteurEntity m WHERE m.prenom like LOWER(:keyword) OR m.nom like LOWER(:keyword)")
     List<MoniteurEntity> searchMoniteursByName(@Param("keyword") String keyword);
+    List<MoniteurEntity> findAllByActifTrue();
+    List<MoniteurEntity> findAllByActifFalse();
 }
