fc7e97ac61f966e439bb18b0a89e6c01ee0a4dd0
ameziel <samuel.sicard.it@gmail.com>
2023-08-20 19:32:15 +0200

    MoniteurServiceImplementation v1

--- src/main/java/tms/tennismanagementsystem/app/moniteur/MoniteurServiceImplementation.java
+++ src/main/java/tms/tennismanagementsystem/app/moniteur/MoniteurServiceImplementation.java
@@ -15,10 +15,15 @@ import java.util.stream.Collectors;
 public class MoniteurServiceImplementation implements MoniteurService{
 
     private MoniteurRepository moniteurRepository;
-
     private MoniteurMapperImplementation moniteurMapperImplementation;
 
-
+    /**
+     * **************************************
+     * *                                    *
+     * *            GET Methods             *
+     * *                                    *
+     * **************************************
+     */
     @Override
     public List<MoniteurDTO> getAllMoniteurs() {
         List<MoniteurEntity> moniteurEntities = moniteurRepository.findAll();
@@ -28,36 +33,12 @@ public class MoniteurServiceImplementation implements MoniteurService{
                 );
         return moniteursDTOS;
     }
-
     @Override
     public MoniteurDTO getMoniteurById(UUID id) throws MoniteurNotFoundException {
         MoniteurEntity moniteurEntity = moniteurRepository.findById(id)
-                .orElseThrow(() -> new MoniteurNotFoundException("Moniteur not found"));
+                .orElseThrow(() -> new MoniteurNotFoundException());
         return moniteurMapperImplementation.fromMoniteurEntity((moniteurEntity));
     }
-
-    @Override
-    public MoniteurDTO saveMoniteur(MoniteurDTO moniteurDTO) {
-        MoniteurEntity moniteurEntity = moniteurMapperImplementation.fromMoniteurDTO(moniteurDTO);
-        MoniteurEntity savedMoniteur = moniteurRepository.save(moniteurEntity);
-        TransformAttributeIntoLowercase(savedMoniteur);
-        return moniteurMapperImplementation.fromMoniteurEntity(savedMoniteur);
-    }
-
-    @Override
-    public MoniteurDTO updateMoniteur(MoniteurDTO moniteurDTO) {
-        MoniteurEntity moniteurEntity = moniteurMapperImplementation.fromMoniteurDTO(moniteurDTO);
-        MoniteurEntity savedMoniteur = moniteurRepository.save(moniteurEntity);
-        TransformAttributeIntoLowercase(savedMoniteur);
-        return moniteurMapperImplementation.fromMoniteurEntity(savedMoniteur);
-    }
-
-    @Override
-    public void deleteMoniteur(UUID id) {
-        moniteurRepository.deleteById(id);
-
-    }
-
     @Override
     public List<MoniteurDTO> searchMoniteursByNames(String keyword) {
         List<MoniteurEntity> moniteurs = moniteurRepository.searchMoniteursByName(keyword);
@@ -65,9 +46,66 @@ public class MoniteurServiceImplementation implements MoniteurService{
                 m -> moniteurMapperImplementation.fromMoniteurEntity(m)).collect(Collectors.toList());
         return moniteurDTOS;
     }
-
-    private void TransformAttributeIntoLowercase(MoniteurEntity savedMoniteur) {
-        savedMoniteur.setNom(savedMoniteur.getNom().toLowerCase());
-        savedMoniteur.setPrenom(savedMoniteur.getPrenom().toLowerCase());
+    public List<MoniteurDTO> getAllActifsMoniteurs() {
+        List<MoniteurEntity> moniteursActifs = moniteurRepository.findAllByActifTrue();
+        List<MoniteurDTO> moniteursActifsDtos = moniteursActifs.stream().map(e -> moniteurMapperImplementation.fromMoniteurEntity(e)).collect(Collectors.toList());
+        return moniteursActifsDtos;
     }
+    public List<MoniteurDTO> getAllInactifsMoniteurs() {
+        List<MoniteurEntity> moniteursInactifs = moniteurRepository.findAllByActifFalse();
+        List<MoniteurDTO> moniteursInactifsDtos = moniteursInactifs.stream().map(e -> moniteurMapperImplementation.fromMoniteurEntity(e)).collect(Collectors.toList());
+        return moniteursInactifsDtos;
+    }
+
+    /**
+     * **************************************
+     * *                                    *
+     * *            POST Methods            *
+     * *                                    *
+     * **************************************
+     */
+    @Override
+    public MoniteurDTO saveMoniteur(MoniteurDTO moniteurDTO) {
+        MoniteurEntity moniteurEntity = moniteurMapperImplementation.fromMoniteurDTO(moniteurDTO);
+        MoniteurEntity savedMoniteur = moniteurRepository.save(moniteurEntity);
+//        TransformAttributeIntoLowercase(savedMoniteur);
+        return moniteurMapperImplementation.fromMoniteurEntity(savedMoniteur);
+    }
+    /**
+     * **************************************
+     * *                                    *
+     * *            PUT Methods             *
+     * *                                    *
+     * **************************************
+     */
+    @Override
+    public MoniteurDTO updateMoniteur(MoniteurDTO moniteurDTO) {
+        MoniteurEntity moniteurEntity = moniteurMapperImplementation.fromMoniteurDTO(moniteurDTO);
+        MoniteurEntity savedMoniteur = moniteurRepository.save(moniteurEntity);
+//        TransformAttributeIntoLowercase(savedMoniteur);
+        return moniteurMapperImplementation.fromMoniteurEntity(savedMoniteur);
+    }
+    /**
+     * **************************************
+     * *                                    *
+     * *           DELETE Methods           *
+     * *                                    *
+     * **************************************
+     */
+    @Override
+    public void deleteMoniteur(UUID id) {
+        moniteurRepository.deleteById(id);
+
+    }
+    /**
+     * **************************************
+     * *                                    *
+     * *        Private Methods             *
+     * *                                    *
+     * **************************************
+     */
+//    private void TransformAttributeIntoLowercase(MoniteurEntity savedMoniteur) {
+//        savedMoniteur.setNom(savedMoniteur.getNom().toLowerCase());
+//        savedMoniteur.setPrenom(savedMoniteur.getPrenom().toLowerCase());
+//    }
 }
