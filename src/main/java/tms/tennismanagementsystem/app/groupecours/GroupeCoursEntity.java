4cd69e019a48f54cb9ef369700bcae117fbe95bc
ameziel <samuel.sicard.it@gmail.com>
2023-08-21 01:08:12 +0200

    correction de la classe "groupecoursEntity"

--- src/main/java/tms/tennismanagementsystem/app/groupecours/GroupeCoursEntity.java
+++ src/main/java/tms/tennismanagementsystem/app/groupecours/GroupeCoursEntity.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import tms.tennismanagementsystem.app.inscriptionformule.InscriptionFormuleEntity;
-import tms.tennismanagementsystem.app.moniteur.MoniteurEntity;
 
 import java.util.UUID;
 
@@ -18,14 +17,8 @@ public class GroupeCoursEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    private String libelle; //Plutot une clé étrangère sur la formule
-    @Enumerated(EnumType.STRING)
-    private Jour jour1;
-    @Enumerated(EnumType.STRING)
-    private Jour jour2;
+    private String libelle;
     private boolean actif;
-    @ManyToOne
-    private MoniteurEntity moniteurResponsable;
     @OneToOne(mappedBy = "referenceGroupe")
     private InscriptionFormuleEntity inscriptionFormuleEntity;
 }
