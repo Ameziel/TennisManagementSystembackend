1171464fb48171db0f9d16294e533933701304b7
ameziel <samuel.sicard.it@gmail.com>
2023-08-23 11:41:24 +0200

    Correction du mapper pour éviter les champs de retour null de la base de données

--- src/main/java/tms/tennismanagementsystem/app/inscriptionsformule/InscriptionFormuleMapperImplementation.java
+++ src/main/java/tms/tennismanagementsystem/app/inscriptionsformule/InscriptionFormuleMapperImplementation.java
@@ -2,6 +2,9 @@ package tms.tennismanagementsystem.app.inscriptionsformule;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import tms.tennismanagementsystem.app.eleves.EleveEntity;
+import tms.tennismanagementsystem.app.formulecours.FormuleCoursEntity;
+import tms.tennismanagementsystem.app.groupescours.GroupeCoursEntity;
 
 @Service
 public class InscriptionFormuleMapperImplementation {
@@ -9,11 +12,28 @@ public class InscriptionFormuleMapperImplementation {
     public InscriptionFormuleDTO fromInscriptionFormuleEntity(InscriptionFormuleEntity inscriptionFormuleEntity) {
         InscriptionFormuleDTO inscriptionFormuleDTO = new InscriptionFormuleDTO();
         BeanUtils.copyProperties(inscriptionFormuleEntity, inscriptionFormuleDTO);
+
+        inscriptionFormuleDTO.setReferenceEleve(inscriptionFormuleEntity.getReferenceEleve().getId());
+        inscriptionFormuleDTO.setReferenceGroupeCours(inscriptionFormuleEntity.getReferenceGroupe().getId());
+        inscriptionFormuleDTO.setReferenceFormuleCours(inscriptionFormuleEntity.getReferenceFormuleCours().getId());
         return inscriptionFormuleDTO;
     }
     public InscriptionFormuleEntity fromInscriptionFormuleDTO(InscriptionFormuleDTO inscriptionFormuleDTO){
         InscriptionFormuleEntity inscriptionFormuleEntity = new InscriptionFormuleEntity();
         BeanUtils.copyProperties(inscriptionFormuleDTO, inscriptionFormuleEntity);
+
+        EleveEntity temp_eleve = new EleveEntity();
+        temp_eleve.setId(inscriptionFormuleDTO.getReferenceEleve());
+        inscriptionFormuleEntity.setReferenceEleve(temp_eleve);
+
+        GroupeCoursEntity temp_groupeCours = new GroupeCoursEntity();
+        temp_groupeCours.setId(inscriptionFormuleDTO.getReferenceGroupeCours());
+        inscriptionFormuleEntity.setReferenceGroupe(temp_groupeCours);
+
+        FormuleCoursEntity temp_formuleCours = new FormuleCoursEntity();
+        temp_formuleCours.setId(inscriptionFormuleDTO.getReferenceFormuleCours());
+        inscriptionFormuleEntity.setReferenceFormuleCours(temp_formuleCours);
+
         return inscriptionFormuleEntity;
     }
 }
